8501b038f5cb980e2d65c20c294b39f2e7f23195
JulsKawa <juvat.2211@gmail.com>
2024-04-24 17:52:14 +0200

    v0.1.4 -> sign & send tx.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
 use elliptic_curve::rand_core::{OsRng, RngCore};
-use secp256k1::{SecretKey, PublicKey, Secp256k1};
-use sha2::{Digest, Sha256};
-use ripemd::{Ripemd160, Digest as RipemdDigest};
 use hex;
+use ripemd::{Digest as RipemdDigest, Ripemd160};
+use secp256k1::{Message, PublicKey, Secp256k1, SecretKey};
+use sha2::{Sha256};
+use serde::{Deserialize, Serialize};
 
 pub struct Wallet {
-    pub sk : SecretKey,
-    pub pk : String,
+    pub sk: SecretKey,
+    pub pk: String,
     pub address: String,
 }
 
@@ -14,8 +15,41 @@ pub struct RPC {
     pub url: String,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+#[derive(Clone)]
+pub struct Model {
+    code: i32,
+    data: Data,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[derive(Clone)]
+#[allow(non_snake_case)]
+pub struct Data {
+    difficulty: f64,
+    hash: String,
+    height: i64,
+    is_janushash: bool,
+    pinHash: String,
+    pinHeight: i64,
+    synced: bool,
+    worksum: f64,
+    worksumHex: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[allow(non_snake_case)]
+pub struct Transaction {
+    pinHeight: u32,
+    nonceId: u32,
+    toAddr: String,
+    amountE8: u64,
+    feeE8: u64,
+    signature65: String,
+}
+
 impl Wallet {
-    pub fn new(sk : Option<String>) -> Wallet {
+    pub fn new(sk: Option<String>) -> Wallet {
         let secret_key = sk.map_or(Self::generate_sk(), |k| k.parse().unwrap());
         let pubkey = Self::generate_pk(secret_key);
         Wallet {
@@ -34,11 +68,11 @@ impl Wallet {
 
     pub fn generate_pk(sk: SecretKey) -> PublicKey {
         let secp = Secp256k1::new();
-        let pk = PublicKey::from_secret_key(&secp,&sk);
+        let pk = PublicKey::from_secret_key(&secp, &sk);
         return pk;
     }
 
-    pub fn generate_address(pubkey : PublicKey) -> String {
+    pub fn generate_address(pubkey: PublicKey) -> String {
         let sha = Sha256::digest(&pubkey.serialize());
         let addr_raw = Ripemd160::digest(&sha);
         let mut hasher_checksum = Sha256::new();
@@ -48,20 +82,71 @@ impl Wallet {
         return hex::encode(addr);
     }
 
-    pub fn sign() {
-        todo!("Implement signing of transaction");
+    pub fn sign_tx(&self, tx_data : Model, to: String, amount : u64, nonce_id: i32, fee_e8: u64) -> String {
+        let pin_height = tx_data.data.pinHeight;
+        let pin_hash = tx_data.data.pinHash;
+
+        let mut to_sign: Vec<u8> = Vec::new();
+        to_sign.extend(hex::decode(pin_hash).unwrap()); // decode hex string to bytes
+        to_sign.extend(&pin_height.to_be_bytes());
+        to_sign.extend(&nonce_id.to_be_bytes());
+        // Fill with 3 bytes of 0
+        to_sign.extend(&[0, 0, 0]);
+        to_sign.extend(&fee_e8.to_be_bytes());
+        // address that we delete 4 bytes of checksum
+        let to_addr_bytes = hex::decode(to).unwrap();
+        to_sign.extend(&to_addr_bytes[0..20]);
+        to_sign.extend(&amount.to_be_bytes());
+
+
+
+        let sha = Sha256::digest(to_sign);
+        let wallet = Wallet::new(Some("dcf9b59a067663f2afacd80eec49af2e8f2aea09507b7f294e25638f35028cf5".to_string()));
+        let sk = wallet.sk;
+
+        let secp = Secp256k1::new();
+        let msg = Message::from_digest_slice(&sha).unwrap();
+        // recoverable signature
+        let sig = secp.sign_ecdsa_recoverable(&msg, &sk);
+
+        let (recid, sig_bytes) = sig.serialize_compact();
+
+        let mut full_sig: Vec<u8> = Vec::new();
+        full_sig.extend_from_slice(&sig_bytes);
+        full_sig.push(recid.to_i32() as u8);
+
+        let signature = hex::encode(full_sig);
+
+        return signature;
+
+
     }
 }
 
 impl RPC {
     pub fn new(url: String) -> RPC {
-        RPC {
-            url,
+        RPC { url }
+    }
+
+    pub fn get_chain_head(&self) -> Result<Model, Box<dyn std::error::Error>> {
+        let url = format!("{}/chain/head", self.url);
+        let resp = reqwest::blocking::get(&url)?;
+        let model: Model = serde_json::from_str(&resp.text()?)?;
+        Ok(model)
+    }
+
+    pub fn send_transaction(&self, transaction: Transaction) -> Result<(), Box<dyn std::error::Error>> {
+        let client = reqwest::blocking::Client::new();
+        let res = client.post(&format!("{}/transaction/add", self.url))
+            .json(&transaction)
+            .send()?;
+
+        if res.status().is_success() {
+            Ok(())
+        } else {
+            Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Failed to send transaction")))
         }
     }
 }
-
 #[cfg(test)]
 mod test;
-
-
--- src/test.rs
+++ src/test.rs
@@ -1,14 +1,60 @@
 #[cfg(test)]
 pub mod tests {
-    use secp256k1::SecretKey;
-    use crate::Wallet;
-
+    use secp256k1::{SecretKey};
+    use crate::{RPC, Transaction, Wallet};
 
     #[test]
     fn test_wallet() {
         let wallet = Wallet::new(None);
-        print!("Secret Key: {:?}\n", SecretKey::clone(&wallet.sk).display_secret());
+        print!(
+            "Secret Key: {:?}\n",
+            SecretKey::clone(&wallet.sk).display_secret()
+        );
         print!("Public Key: {:?}\n", wallet.pk);
         print!("Address: {:?}\n", wallet.address);
+    }
+
+    #[test]
+    fn test_wallet_with_sk() {
+        let wallet = Wallet::new(Some("dcf9b59a067663f2afacd80eec49af2e8f2aea09507b7f294e25638f35028cf5".to_string()));
+        println!("{:?}", wallet.address)
+    }
+
+    #[test]
+    fn test_rpc() {
+        let rpc = RPC {
+            url: "http://51.75.21.134:3001".to_string(),
+        };
+        let result = rpc.get_chain_head();
+        print!("{:?}", result);
+    }
+
+    #[test]
+    fn test_send() {
+        let rpc = RPC {
+            url: "http://51.75.21.134:3100".to_string(),
+        };
+        let data = rpc.get_chain_head().unwrap();
+        let pin_height = &data.data.pinHeight;
+        let wallet = Wallet::new(Some("dcf9b59a067663f2afacd80eec49af2e8f2aea09507b7f294e25638f35028cf5".to_string()));
+        let nonce_id: i32 = 1;
+        let to = "0000000000000000000000000000000000000000de47c9b2".to_string();
+        let amount_e8 = 1;
+        let fee_e8 = 1;
+
+        let sig = wallet.sign_tx(data.clone(), to.clone(), amount_e8, nonce_id, fee_e8);
+
+        let tx = Transaction {
+            pinHeight: *pin_height as u32,
+            nonceId: nonce_id as u32,
+            toAddr: to,
+            amountE8: amount_e8,
+            feeE8: fee_e8,
+            signature65: sig,
+        };
+
+        let result = rpc.send_transaction(tx);
+
+        print!("{:?}", result);
     }
 }
